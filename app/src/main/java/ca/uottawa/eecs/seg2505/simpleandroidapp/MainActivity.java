ae3a7a8a225915475774aba717b3dff51050fe98
Robert Laganiere <robert@laganiere.name>
2015-06-01 17:07:20 -0400

    Initial commit

---
+++ app/src/main/java/ca/uottawa/eecs/seg2505/simpleandroidapp/MainActivity.java
@@ -0,0 +1,56 @@
+/**
+ * This file contains material supporting the course
+ * SEG2505: Introduction to Software Engineering
+ * University of Ottawa.
+ * http://wwww.eecs.uottawa.ca/~laganier
+ *
+ * This program is free software; permission is hereby granted to use, copy, modify,
+ * and distribute this source code, or portions thereof, for any purpose, without fee,
+ * subject to the restriction that the copyright notice may not be removed
+ * or altered from any source or altered source distribution.
+ * The software is released on an as-is basis and without any warranties of any kind.
+ * In particular, the software is not guaranteed to be fault-tolerant or free from failure.
+ * The author disclaims all warranties with regard to this software, any use,
+ * and any consequent failure, is purely the responsibility of the user.
+ *
+ * Hanna Farah 2015
+ * Robert Laganiere 2015
+ */
+package ca.uottawa.eecs.seg2505.simpleandroidapp;
+
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
+
+
+public class MainActivity extends ActionBarActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
